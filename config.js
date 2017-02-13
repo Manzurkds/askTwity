7cd9103857340ef75f5927bc4b8faa76edd6e6cb
Manzur <manzurkds@gmail.com>
2017-02-13 17:20:12 +0530

    removed config

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	consumer_key:         'NZ0BSsf08UKlhbweo6TktcUCa',
-  consumer_secret:      'TnEhyiRVFtu2Go67ebfu699Bw6WjxTgmXkuKxv5FWNoAa5ewL5',
-  access_token:         '831071469912281088-95jOd1bTP9cKHeS2EnTyipvLOYwLnp0',
-  access_token_secret:  '9jkGmwA7TP0ZCcsDuOAH8n9i6Jr6FZrbzIY1egLuVnMRY'
-  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-}
