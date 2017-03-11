17254f1c9a2e58f26a9b1c6768dcb1ae9f000d67
Manzur Khan Sarguru <manzurkds@gmail.com>
2017-03-11 22:58:28 +0530

    removed config from gitignore

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	consumer_key:         'TLKDILR4O7BsTM84D9hLEcQgo',
+  consumer_secret:      'XRSnXNemIjact815JAHstjDcT9r98QwKIp9RgFdbnAwgNVihlR',
+  access_token:         '831071469912281088-8O5bHRzdrTEckRL4iPt2VkXxxJi0M1u',
+  access_token_secret:  'inPDwVXZgJ0MhNHWWQY9itEZDfNBwMJQ8CYybnUUsbhuY',
+  open_weather_API: 	'10690dbc44dd8ff6a06536eef8f63727'
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
